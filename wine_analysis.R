1d3b0956e51b52d04847f2b45fdfd138d70d8477
Carson <cforter@users.noreply.github.com>
2015-03-20 14:52:50 -0700

    Update wine_analysis.R

--- wine_analysis.R
+++ wine_analysis.R
@@ -10,9 +10,9 @@ lm_cheap <- lm(rating_cheap ~ treatment, data=data)
 
 # Model with all price points and heterogeneous treatment effects, cheap = base-level factor
 # Raw treatment coefficient represents treatment effect for the cheap wine
-# Not fully saturated, but we might to condiser adding interaction terms for geneder etc.
+# Not fully saturated, but we might want to consider adding interaction terms for geneder etc.
 # in case there are other heterogeneous effects
-# We'll need to melt data and restructure so each wine rating its own record
+# We'll need to melt data and restructure so each wine rating is its own record
 lm.all <- lm(rating_all ~ treatment + quiz_score + zip_code
              + age + gender + group + mid_dummy + expensive_dummy
              + (mid_dummy * treatment) + (expensive_dummy * treatment), data=data)
